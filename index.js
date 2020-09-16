f89f5fba5cf746c48be30a74e1bffda9e148edd0
Sami Jouppila <sami.jouppila@gmail.com>
2020-09-16 15:34:08 +0300

    Added routes and controllers for GET, PUT /users/:id and POST /users

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000
-const apiRouter = require('./routes');
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.status(200).send("Weather Station Application root. See /api");
+  res.status(200).send("Weather Station application root. See /api");
 })
+
+const apiRouter = require('./routes');
 
 app.use('/api', apiRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Weather station listening at http://localhost:${port}`)
 })
--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,22 @@ const userRouter = express.Router();
 
 const {
     getAllUsers,
-    createNewUser
+    getSelectedUser,
+    createNewUser,
+    modifyExistingUser
 } = require('../controllers/userController');
+
+const {
+    validateUserCreationRequestBody,
+    validateUserModificationRequestBody
+} = require('../validators/userValidator');
 
 userRouter.get('', getAllUsers);
 
-userRouter.post('', createNewUser);
+userRouter.get('/:id', getSelectedUser);
+
+userRouter.post('', validateUserCreationRequestBody, createNewUser);
+
+userRouter.put('/:id', validateUserModificationRequestBody, modifyExistingUser);
 
 module.exports = userRouter;
